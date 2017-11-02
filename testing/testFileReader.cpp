a2b1a3150976c11f3bf87d2007df2468acaaabc2
Lawson Johnson <lawson.johnson@richmond.edu>
2017-11-02 16:39:46 -0400

    Rebuilt FileReader
    Altered Filereader header for less complexity and rebuilt the functionality of FileReader completely, passing tester file is included

--- testing/testFileReader.cpp
+++ testing/testFileReader.cpp
@@ -19,25 +19,29 @@ int main(){
     cout << "Testing FileReader(string filename)\n";
     cout << br;
     try{
-        FileReader config("config.cfg");
+        FileReader configTest("config.cfg");
     }catch(...){
         cout << fail;
         throw;
         return 1;
     }
+    FileReader config("config.cfg");
     cout << pass;
     cout << br;
-    cout << "Listing Environment Variables\n";
+    cout << "Listing Environment Variables (get Methods)\n";
     cout << br;
-    cout << getenv("program_input") << endl;
-    cout << getenv("memory_contents_input") << endl;
-    cout << getenv("register_file_input") << endl;
-    cout << getenv("output_file") << endl;
-    cout << getenv("output_mode") << endl;
-    cout << getenv("debug_mode") << endl;
-    cout << getenv("print_memory_contents") << endl;
-    cout << getenv("write_to_file") << endl;
-    cout << getenv("java") << endl;
+    cout << "program_input = "         << config.getInstMem()   << endl;
+    cout << "memory_contents_input = " << config.getDataMem()   << endl;
+    cout << "register_file_input = "   << config.getRegFile()   << endl;
+    cout << "output_file = "           << config.getOutFile()   << endl;
+    cout << "output_mode = "           << config.getOutMode()   << endl;
+    cout << "debug_mode = "            << config.getDebugMode() << endl;
+    cout << "print_memory_contents = " << config.getPrintMem()  << endl;
+    cout << "write_to_file = "         << config.writeToFile()  << endl;
+    cout << br;
+    cout << pass;
+    cout << br;
+    cout << "Testing complete\n";
     cout << br;
 //===============================================================================================//
 
